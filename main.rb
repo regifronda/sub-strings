044742a37b6ad036676aa5989e3c44231cf74111
Regi Fronda <regisfronda@gmail.com>
2021-06-15 02:46:36 -0700

    Creae results_hash that checks if array strings includes substrings

--- main.rb
+++ main.rb
@@ -1,11 +1,16 @@
-require 'pry'
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings(string, substrings)
-  string = string.downcase.split(" ")
-  binding.pry
-  result_hash = Hash.new
-  result_hash.default = 0  
+def substrings(strings, compare_substrings)
+  strings = strings.downcase.split(" ")
+  results_hash = strings.reduce(Hash.new(0)) do |results, string|
+    compare_substrings.each do |compare_substring|
+      if string.include?(compare_substring)
+        results[compare_substring] += 1
+      end
+    end
+    results
+  end
+  results_hash
 end
 
-substrings("below downtown", dictionary)
+puts substrings("below downtown", dictionary)
