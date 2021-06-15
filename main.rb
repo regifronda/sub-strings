941714c58950a9ed6cbef5e93e9a7296042f40c7
Regi Fronda <regisfronda@gmail.com>
2021-06-15 01:50:07 -0700

    Change string into array of words

--- main.rb
+++ main.rb
@@ -1,9 +1,11 @@
+require 'pry'
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string, substrings)
-  result = Hash.new
-  result.default = 0
-  new_string = string.downcase.delete(",.! ").split
-  
+  string = string.downcase.split(" ")
+  binding.pry
+  result_hash = Hash.new
+  result_hash.default = 0  
 end
 
+substrings("below downtown", dictionary)
